25eacad6bba0e04511a56b27d1136d87399b8de4
hsjhsh14 <hsjhsh4182@gmail.com>
2018-03-13 09:22:06 +0900

    올라가 주세요!!!!!dfgdfg

--- app/src/main/java/com/example/user/fghnfggfgh/Episode_Activity.java
+++ app/src/main/java/com/example/user/fghnfggfgh/Episode_Activity.java
@@ -23,7 +23,7 @@ public class Episode_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_episode);
-    //gitTest 올라가 주세요 !! 메로로로롱
+    //gitTest 올라가 주세요 !! 메로로로롱dfvdcxv
        // toolbar_activity_episode = findViewById(R.id.ep)
 
         episode_activity_fragement_best = new Episode_Activity_fragement_best();
